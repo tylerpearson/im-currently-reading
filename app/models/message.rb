fbbcf1aed8d8bb5629710d10d47f1539b37867f1
Tyler Pearson <ty.pearson@gmail.com>
2013-05-13 01:06:42 -0400

    set max length for slug generation

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,7 +7,7 @@ class Message < ActiveRecord::Base
   friendly_id :custom_slug, use: :slugged
 
   def custom_slug
-    "#{self.book.author_name}-#{self.book.title}"
+    "#{self.book.title.truncate(50, :separator => ' ')}-by-#{self.book.author_name}"
   end
 
 end
