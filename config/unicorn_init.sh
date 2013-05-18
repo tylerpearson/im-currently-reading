f0dbbe94a8f80f60672b46529a8833b9311f7a70
Tyler Pearson <ty.pearson@gmail.com>
2013-05-18 18:57:49 -0400

    set path relative to init directory

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -6,7 +6,7 @@ TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/imcurrentlyreading/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 
-. env_vars.sh
+. ./env_vars.sh
 
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deployer
